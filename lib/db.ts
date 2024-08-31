0248f1768906e47ae3656a6b934b8e5a85fbf73d
ABHIRUP <abhirupsaha02@gmail.com>
2024-09-01 01:19:30 +0530

    prisma update

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,20 @@
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
+
+export const db = globalThis.prisma || new PrismaClient({
+    adapter,
+    log: ['info', 'warn', 'error']
+});
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
